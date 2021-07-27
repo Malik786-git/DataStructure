1887bdc72cc2c42111adf070ded75f6d1fbf19f2
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-07-27 17:35:37 +0500

    add arrays

---
+++ 1_Arrays/0_implementCustumArrays.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class intArray
+{
+    int totalSize;
+    int usedSize;
+    int *ptr;
+
+public:
+    intArray(int size, int used)
+    { //initialize array size and memory dynamically
+        totalSize = size;
+        usedSize = used;
+        ptr = new int(size);
+    }
+    ~intArray()
+    { //free memoty
+        delete ptr;
+    }
+    void setData()
+    { //input data
+        for (int i = 0; i < usedSize; i++)
+        {
+            cout << "Enter Data = ";
+            cin >> (ptr)[i];
+        }
+    }
+    void disply()
+    { //display data Or Traversal
+        for (int i = 0; i < usedSize; i++)
+        {
+            cout << "index " << i << " = " << (ptr)[i] << endl;
+        }
+    }
+};
+
+int main()
+{
+
+    intArray marks(100, 5);
+    marks.setData();
+    cout << endl;
+    marks.disply();
+
+    return 0;
+}
---
+++ 1_Arrays/1_insertionInArray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+// Basic insertion To understand the Concept
+int sortedInsertion(int arr[], int size, int element, int capacity, int index)
+{
+    if (size >= capacity)
+    {
+        return -1;
+    }
+    for (int i = size-1; i >= index; i--)
+    {
+        arr[i+1] = arr[i];
+
+    }
+    arr[index] = element;
+    return 1;
+}
+int main()
+{
+    int arr[100] = {1, 2, 6, 78, 88};
+    int size = 5, element = 45, index = 3;
+    display(arr, size);
+    sortedInsertion(arr, size, element, 100, index);
+    cout<<endl;
+    display(arr, size+1);
+
+    return 0;
+}
